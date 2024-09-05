ed7e69c6fd7849dc1b11ebdc8010de669748fb45
truptidhanraj <trupti.nikam3@gmail.com>
2024-09-05 15:59:15 +0000

    to clone

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import React from 'react'
+
+import './index.css'
+
+const TodoItem = props => {
+  const {details, deleteTodoItem} = props
+  const {id, title} = details
+
+  const handleClick = () => {
+    deleteTodoItem(id)
+  }
+  
+  return (
+    <li>
+      <p>{title}</p>
+      <div>
+        <button onClick={handleClick}>Delete</button>
+      </div>
+    </li>
+  )
+}
+
+export default TodoItem
